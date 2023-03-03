1fca0f630aa1764a2a613dc99df1e58ac091f1bc
CrisVillatoro <villatorocrista@gmail.com>
2023-03-03 14:16:36 +0100

    simulation with GIF

--- final_shoppers_simulations.py
+++ final_shoppers_simulations.py
@@ -9,6 +9,27 @@ import time
 from PIL import Image
 from create_charts import create_charts
 from create_charts import create_folder
+import imageio
+#%%
+
+def add_element_and_get_index(element, element_list):
+    """
+    Checks if an element is in a list and adds it to the list if not.
+    Returns the index of the element in the list.
+    Args:
+        element (str):
+            Element to be added to the list
+        element_list (list):
+            List to add the element to
+    Returns:
+         Index of inserted element in the list
+    """
+    if element not in element_list:
+        element_list.append(element)
+    return element_list.index(element)
+
+
+time_list = []
 
 # represantation of the market with one symbol '#', '.', 'b' or similar for each tile on the png-file
 MARKET ="""
@@ -29,10 +50,15 @@ MARKET ="""
 # Path where the file has to be saved
 parent_directory =  './images'
 chart_directory = 'charts_customers'
-filepath = create_folder(parent_directory, chart_directory)
+charts_filepath = create_folder(parent_directory, chart_directory)
+
+# Path for animation images
+parent_directory =  './images'
+chart_directory = 'charts_gif'
+charts_filepath = create_folder(parent_directory, chart_directory)
 
 # Define the background of the gif
-background = np.ones((1000, 730, 3), np.uint8)
+background = np.ones((1000, 930, 3), np.uint8)
 background = 255 * background
 
 # Read the tiles png
@@ -58,7 +84,7 @@ lineType2               = 2
 supermarket_map = SupermarketMap(MARKET, tiles)
 
 # Instantiate the Supermarket object
-Tahini_supermarket = Supermarket("Amazing Tahini", 7, "07:08:00", 1.6) # Can change the time based on user
+Tahini_supermarket = Supermarket("NonStopShopper!", 7, "08:08:00", 1.6) # Can change the time based on user
 print(Tahini_supermarket.name)
 
 # Size of a tile (32*32 pixels = 1 tile)
@@ -98,6 +124,7 @@ with open (f"final_shoppers.csv", "w", encoding = 'UTF8', newline="") as f:
         Tahini_supermarket.get_text(frame, bottomLeftCornerOfText2, font2, fontScale2, fontColor2,thickness2, lineType2)
         current_time = Tahini_supermarket.get_time()
         print(current_time)
+        add_element_and_get_index(current_time, time_list)
         time.sleep(0.5)
 
         if len(final_customers) != 0:
@@ -124,8 +151,16 @@ with open (f"final_shoppers.csv", "w", encoding = 'UTF8', newline="") as f:
             offset = np.array((image_y, 40))  # Top-left point from which to insert the smallest image. height first, from the top of the window
             frame[offset[0]:offset[0] + height, offset[1]:offset[1]+width] = smallImage
 
+            def rescale_frame(frame, percent=200):
+                width = int(frame.shape[1] * percent/ 100)
+                height = int(frame.shape[0] * percent/ 100)
+                dim = (width, height)
+                return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
             # Display the frame
-            cv2.imshow("frame", frame)
+            frame_rescaled = rescale_frame(frame)
+            cv2.imshow("frame", frame_rescaled)
+            cv2.imwrite(f'./images/charts_gif/final_{current_time}.png',frame_rescaled)
 
         else:
             continue
@@ -135,4 +170,14 @@ with open (f"final_shoppers.csv", "w", encoding = 'UTF8', newline="") as f:
     supermarket_map.write_image("NonStopShopper.png")
     f.close
 
+#Generate the GIF
+
+
+images = []
+
+for value_time in time_list:
+    filename = './images/charts_gif/final_{}.png'.format(value_time)
+    images.append(imageio.imread(filename))
+
+imageio.mimsave('output_final.gif', images, fps=1)
 # %%
